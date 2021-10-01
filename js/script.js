ac43c8b454b4094ee23e05062be0c9c7c902a5b2
Andrea Falzi <andrea.falzi1988@gmail.com>
2021-10-01 14:27:00 +0200

    header animation + slider

--- js/script.js
+++ js/script.js
@@ -2,6 +2,35 @@
 
 /*========FUNCTIONS========*/
 
-new Glide(".glide", {
-  type: "carousel",
-}).mount();
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
+
+
+
+const squares = document.querySelectorAll(".square");
+
+setInterval(() => {
+  squares.forEach((square) => {
+    square.classList.toggle("xyz-in");
+    square.classList.toggle("xyz-out");
+  });
+}, 1200);
