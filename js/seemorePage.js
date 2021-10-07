73ddbc4818cec0e3cbc3600a29d571ff6e27aa0f
andreafalzi <andrea.falzi1988@gmail.com>
2021-10-07 13:20:56 +0200

    fix contact form z-index

--- js/seemorePage.js
+++ js/seemorePage.js
@@ -4,7 +4,31 @@ const arrayWorks = [
     title: 'Urbangreen',
     thumbnails: '',
     description: ''
-  }
+  },
+  {
+    id: '2',
+    title: 'Big.byte',
+    thumbnails: '',
+    description: ''
+  },
+  {
+    id: '3',
+    title: 'Mario Jump',
+    thumbnails: '',
+    description: ''
+  },
+  {
+    id: '4',
+    title: 'Irest',
+    thumbnails: '',
+    description: ''
+  },
+  {
+    id: '5',
+    title: 'Beer POG',
+    thumbnails: '',
+    description: ''
+  },
 ]
 
 /*========GLOBAL VARIABLES========*/
