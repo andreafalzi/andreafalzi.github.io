d5a4ac42102886283098d1323affe75c12404c33
Andrea Falzi <andrea.falzi1988@gmail.com>
2021-10-12 00:40:49 +0200

    responsive + svg icon position + slider rework

--- js/index.js
+++ js/index.js
@@ -175,6 +175,44 @@ const swiper = new Swiper(".swiper", {
     el: ".swiper-scrollbar",
   },
 });
+const swiperDesktop = new Swiper(".swiper-desktop", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: false,
+  effect: "slide",
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    900: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    // when window width is >= 480px
+    1100: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+  },
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+});
 
 /* SMOOTH SCROLL */
 $(document).ready(function () {
