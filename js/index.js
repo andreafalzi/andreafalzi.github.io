7871dd84cc085b1d9440bc9bb8b23c0c1af4ece1
Andrea Falzi <andrea.falzi1988@gmail.com>
2021-12-28 00:28:14 +0100

    back to jquery

--- js/index.js
+++ js/index.js
@@ -185,49 +185,32 @@ function loader() {
 /*========EXTERNALS========*/
 
 /* SMOOTH SCROLL */
-// $(document).ready(function () {
-//   // Add smooth scrolling to all links
-//   $("a").on("click", function (event) {
-//     // Make sure this.hash has a value before overriding default behavior
-//     if (this.hash !== "") {
-//       // Prevent default anchor click behavior
-//       event.preventDefault();
+$(document).ready(function () {
+  // Add smooth scrolling to all links
+  $("a").on("click", function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
 
-//       // Store hash
-//       var hash = this.hash;
+      // Store hash
+      var hash = this.hash;
 
-//       // Using jQuery's animate() method to add smooth page scroll
-//       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-//       $("html, body").animate(
-//         {
-//           scrollTop: $(hash).offset().top,
-//         },
-//         800,
-//         function () {
-//           // Add hash (#) to URL when done scrolling (default click behavior)
-//           window.location.hash = hash;
-//         }
-//       );
-//     } // End if
-//   });
-// });
-// Add smooth scrolling to all links
-const links = document.querySelectorAll("[data-nav-anchor]");
-
-for (const link of links) {
-  link.addEventListener("click", clickHandler);
-}
-
-function clickHandler(e) {
-  e.preventDefault();
-  const href = this.getAttribute("href");
-  const offsetTop = document.querySelector(href).offsetTop;
-
-  scroll({
-    top: offsetTop + 75,
-    behavior: "smooth",
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        function () {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        }
+      );
+    } // End if
   });
-}
+});
 
 /* SLIDER */
 if (document.querySelector(".swiper")) {
